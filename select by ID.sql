e6e1c8886e322b354280408a9ed7f57c11cbe7f6
Yashvardhan sharma <95120981+Yvsharma@users.noreply.github.com>
2023-01-24 12:22:27 +0530

    Select by ID

---
+++ select by ID.sql
@@ -0,0 +1,11 @@
+--  to select all column we use *
+
+select * from city
+
+-- to see data with specific ID
+
+where ID = '1661';
+-- using where clause
+-- sytax
+-- where colum name with specific condition
+-- where is used to give codition.
